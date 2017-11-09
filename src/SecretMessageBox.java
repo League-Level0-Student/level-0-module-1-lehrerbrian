d17211a57c2ed4a89af163903ac959f9cf56d6a3
lehrerbrian <league@WTS21.attlocal.net>
2017-11-08 16:54:05 -0800

    new stuff

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -1,32 +1,36 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
 
 import javax.swing.JOptionPane;
 
 /**
  * Secret Message Box / Secure Messaging System
  * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
+ * You want to leave a message on one of the Mac computers so that only your
+ * friend can read it. You set up the passcode and the secret message. Your
+ * friend types in the passcode to retrieve the message.
  * 
  */
 
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
-	
-	// 1. Set the passcode in a String variable
-	
-	// 2. Using a pop-up, ask for a secret message and store it in a variable
+	public static void main(String[] args) {
 
-	// 3. Ask your friend for the passcode and store it in a variable
-
-	// 4. If the passcode matches, show the secret message
-
-	// 5. If the passcode does not match, pop-up "INTRUDER!!"
-
-	// [optional] 6. Have your friend also enter a username, and make sure it is correct before releasing the secret message.
-
+		// 1. Set the passcode in a String variable
+		String pw = "password";
+		// 2. Using a pop-up, ask for a secret message and store it in a variable
+		String message = JOptionPane.showInputDialog("whats the message?");
+		// 3. Ask your friend for the passcode and store it in a variable
+		String guess = JOptionPane.showInputDialog("whats the password ?");
+		// 4. If the passcode matches, show the secret message
+		if (guess.equals(pw)) {
+			JOptionPane.showMessageDialog(null, message);
+		} else JOptionPane.showMessageDialog(null, "INTRUDER");
+		// 5. If the passcode does not match, pop-up "INTRUDER!!"
+		
+		// [optional] 6. Have your friend also enter a username, and make sure it is
+		// correct before releasing the secret message.
+	}
 }
--- src/remarkable.java
+++ src/remarkable.java
@@ -1,4 +1,21 @@
+import javax.swing.JOptionPane;
 
 public class remarkable {
-
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("What is your name?");
+		if (name.equalsIgnoreCase("brian")) {
+			JOptionPane.showConfirmDialog(null, "Brian you have blue eyes right?");
+			
+		} else if (name.equalsIgnoreCase("izzy")) {
+			JOptionPane.showConfirmDialog(null, "Izzy you have brown eyes right?"); 
+		} else if (name.equalsIgnoreCase("elijah")) {
+			JOptionPane.showConfirmDialog(null, "Elijah you have brown eyes right?");
+		}else if (name.equalsIgnoreCase("kamron")) {
+			JOptionPane.showConfirmDialog(null, "Kamron you have brown eyes right?");
+		}else if (name.equalsIgnoreCase("jackie")) {
+			JOptionPane.showConfirmDialog(null, "Jackie you have blue eyes right?");
+		}else if (name.equalsIgnoreCase("ian")) {
+			JOptionPane.showConfirmDialog(null, "Ian you have brown and blue eyes right?");
+		}
+	}
 }
